cb2580b4fb846e0e71583888be9b01e3005b88e6
student <preethi_b@hcl.com>
2021-04-18 02:42:29 +0000

    first commit

---
+++ MavenDevOps/src/main/java/com/preethi/MavenDevOps/App.java
@@ -0,0 +1,13 @@
+package com.preethi.MavenDevOps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Dev-Ops" );
+    }
+}
